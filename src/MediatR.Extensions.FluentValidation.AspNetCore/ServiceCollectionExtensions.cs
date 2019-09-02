eb532bf5c18ae6c75969da7a3efe9996cb699889
mprzybylek <getoxs@gmail.com>
2019-09-03 01:20:05 +0200

    Add src

---
+++ src/MediatR.Extensions.FluentValidation.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using FluentValidation;
+using System.Linq;
+
+namespace MediatR.Extensions.FluentValidation.AspNetCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddFluentValidation(this IServiceCollection services, IEnumerable<Assembly> assemblies, ServiceLifetime lifetime = ServiceLifetime.Transient)
+        {
+            services.RegisterAllTypesGeneric(typeof(IPipelineBehavior<,>), assemblies, lifetime);
+
+            services.AddValidatorsFromAssemblies(assemblies, lifetime);
+        }
+
+        public static void RegisterAllTypesGeneric(this IServiceCollection services, Type t, IEnumerable<Assembly> assemblies,
+            ServiceLifetime lifetime = ServiceLifetime.Transient)
+        {
+            var genericType = t;
+            var typesFromAssemblies = assemblies.SelectMany(a => a.DefinedTypes.Where(x => x.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericType)));
+
+            foreach (var type in typesFromAssemblies)
+            {
+                services.Add(new ServiceDescriptor(t, type, lifetime));
+            }
+        }
+    }
+}
---
+++ src/MediatR.Extensions.FluentValidation.AspNetCore/ValidationBehavior.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatR.Extensions.FluentValidation.AspNetCore
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request
+            , CancellationToken cancellationToken
+            , RequestHandlerDelegate<TResponse> next
+        )
+        {
+            var context = new ValidationContext(request);
+            var failures = _validators
+                .Select(v => v.Validate(context))
+                .SelectMany(result => result.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+
+            return next();
+        }
+    }
+}
