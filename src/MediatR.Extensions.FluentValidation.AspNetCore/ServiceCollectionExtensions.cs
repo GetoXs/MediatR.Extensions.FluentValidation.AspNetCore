d8234deae893ac892bddb7e2eb036e39a6d263a4
mprzybylek <getoxs@gmail.com>
2019-09-03 09:51:32 +0200

    Lower dependency, Code cleanup

--- src/MediatR.Extensions.FluentValidation.AspNetCore/ServiceCollectionExtensions.cs
+++ src/MediatR.Extensions.FluentValidation.AspNetCore/ServiceCollectionExtensions.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
 using System.Reflection;
-using FluentValidation;
-using System.Linq;
 
 namespace MediatR.Extensions.FluentValidation.AspNetCore
 {
@@ -11,22 +9,9 @@ namespace MediatR.Extensions.FluentValidation.AspNetCore
     {
         public static void AddFluentValidation(this IServiceCollection services, IEnumerable<Assembly> assemblies, ServiceLifetime lifetime = ServiceLifetime.Transient)
         {
-            services.RegisterAllTypesGeneric(typeof(IPipelineBehavior<,>), assemblies, lifetime);
+            services.Add(new ServiceDescriptor(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>), lifetime));
 
             services.AddValidatorsFromAssemblies(assemblies, lifetime);
-        }
-
-        public static void RegisterAllTypesGeneric(this IServiceCollection services, Type t, IEnumerable<Assembly> assemblies,
-            ServiceLifetime lifetime = ServiceLifetime.Transient)
-        {
-            var genericType = t;
-            var typesFromAssemblies = assemblies.SelectMany(a => a.DefinedTypes.Where(x => x.GetInterfaces()
-                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericType)));
-
-            foreach (var type in typesFromAssemblies)
-            {
-                services.Add(new ServiceDescriptor(t, type, lifetime));
-            }
         }
     }
 }
