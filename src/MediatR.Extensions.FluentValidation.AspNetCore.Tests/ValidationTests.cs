ccec078eed40204d629c51b46bd1f84f8cdc2633
Mateusz Przybyłek <getoxs@gmail.com>
2020-08-06 00:48:40 +0200

    Add test project

---
+++ src/MediatR.Extensions.FluentValidation.AspNetCore.Tests/ValidationTests.cs
@@ -0,0 +1,68 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MediatR.Extensions.FluentValidation.AspNetCore.Tests
+{
+    public class ValidationTests
+    {
+        private readonly ServiceProvider Services;
+
+        public readonly IMediator Mediator;
+
+        public ValidationTests()
+        {
+            ServiceCollection sc = new ServiceCollection();
+
+            var domainAssembly = typeof(ValidationTests).GetTypeInfo().Assembly;
+
+            // Add MediatR
+            sc.AddMediatR(domainAssembly);
+
+            //Add FluentValidation
+            sc.AddFluentValidation(new[] { domainAssembly });
+
+            Services = sc.BuildServiceProvider();
+            Mediator = Services.GetService<IMediator>();
+        }
+
+        [Fact]
+        public async Task CheckForValidationPass()
+        {
+            await Mediator.Send(new Command { Discount = 1 });
+        }
+
+        [Fact]
+        public async Task CheckForValidationFailure()
+        {
+            await Assert.ThrowsAsync<ValidationException>(() => Mediator.Send(new Command { Discount = -1 }));
+        }
+    }
+
+    public class CommandValidation : AbstractValidator<Command>
+    {
+        public CommandValidation()
+        {
+            RuleFor(x => x.Discount).GreaterThanOrEqualTo(0);
+        }
+    }
+
+
+    public class Command : IRequest
+    {
+        public decimal Discount { get; set; }
+
+    }
+
+    public class CommandHandler : IRequestHandler<Command>
+    {
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+            return await Unit.Task;
+        }
+
+    }
+}
