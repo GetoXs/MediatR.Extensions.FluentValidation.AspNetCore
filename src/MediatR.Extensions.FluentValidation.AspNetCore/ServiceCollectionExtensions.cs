f1f3116846d4c74cab39219f915c6147467b0f1c
Mateusz Przybyłek <getoxs@gmail.com>
2023-11-19 21:11:40 +0100

    Add includeInternalTypes parameter #18

--- src/MediatR.Extensions.FluentValidation.AspNetCore/ServiceCollectionExtensions.cs
+++ src/MediatR.Extensions.FluentValidation.AspNetCore/ServiceCollectionExtensions.cs
@@ -12,12 +12,13 @@ namespace MediatR.Extensions.FluentValidation.AspNetCore
             this IServiceCollection services, 
             IEnumerable<Assembly> assemblies, 
             ServiceLifetime lifetime = ServiceLifetime.Transient,
-            Func<AssemblyScanner.AssemblyScanResult, bool> filter = null
+            Func<AssemblyScanner.AssemblyScanResult, bool> filter = null,
+            bool includeInternalTypes = false
             )
         {
             services.Add(new ServiceDescriptor(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>), lifetime));
 
-            services.AddValidatorsFromAssemblies(assemblies, lifetime, filter);
+            services.AddValidatorsFromAssemblies(assemblies, lifetime, filter, includeInternalTypes);
             
             return services;
         }
